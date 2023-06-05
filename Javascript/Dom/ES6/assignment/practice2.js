88865bf03d6d6d77ec5427e5a4cd721fe9dbc6a1
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2023-06-05 14:50:39 +0530

    'hii'

---
+++ Javascript/Dom/ES6/assignment/practice2.js
@@ -0,0 +1,10 @@
+//check if the value exists in array in javascript
+var arr=["manu","mani","manasa","modi"];
+if(arr.indexOf("man")!==-1){
+    console.log("yes");
+}
+
+else
+{
+console.log("no");
+}
---
+++ Javascript/Dom/ES6/practice1.js
@@ -0,0 +1,7 @@
+var arr=["manu","mani","manasa","modi","mani"];
+ function filter(new__arr){
+    return new__arr.filter((value,index)=>new__arr.indexOf(value)===index);
+
+    
+ }
+ console.log(filter(arr));
